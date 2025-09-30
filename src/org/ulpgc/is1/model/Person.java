688eae0c88ad98c0bca890a1137a20d1f15d85fe
David <david.monzon103@alu.ulpgc.es>
2025-09-30 20:03:40 +0100

    Crear atributos y métodos en las clases

--- src/org/ulpgc/is1/model/Person.java
+++ src/org/ulpgc/is1/model/Person.java
@@ -1,13 +1,13 @@
 package org.ulpgc.is1.model;
 
 public class Person extends Contact {
-    private String name;
-    private String description;
+    private String firstName;
+    private String lastName;
 
-    public Person(String telephone, String email, Address address, String name, String description) {
+    public Person(String telephone, String email, Address address, String firstName, String lastName) {
         super(telephone, email, address);
-        this.name = name;
-        this.description = description;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     @Override
